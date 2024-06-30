7c7870f999e652b2e8674ffb0595dbe2452fc25c
Anya <anyagoremykina@gmail.com>
2024-06-30 18:36:44 +0000

    add precommit

--- setup.py
+++ setup.py
@@ -4,8 +4,7 @@ setup(
     name="custom-batchnorm",
     packages=find_packages(),
     version="0.1.0",
-    install_requires=[
-        "torch==2.2.1+cu121"
-    ],
+    install_requires=["torch==2.2.1+cu121"],
+    extras_require={"dev": ["pre-commit", "pytest", "flake8", "black"]},
     license="MIT",
 )
